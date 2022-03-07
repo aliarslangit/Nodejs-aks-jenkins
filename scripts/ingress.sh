cc9e0d8f56f65e0c3f3914a12633570b7c665ffb
Ali Arslan <aliarslanmushtaqali@gmail.com>
2022-03-07 12:34:01 +0500

    Update ingress.sh

--- scripts/ingress.sh
+++ scripts/ingress.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Create a namespace for your ingress resources
-kubectl create namespace ingress-basic
 
-helm init
-# Add the ingress-nginx repository
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-
-# Use Helm to deploy an NGINX ingress controller
-helm install nginx-ingress ingress-nginx/ingress-nginx \
-    --namespace ingress-basic \
-    --set controller.replicaCount=2 \
-    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
-    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
-    --set controller.admissionWebhooks.patch.nodeSelector."beta\.kubernetes\.io/os"=linux
-
+# deploy an NGINX ingress controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
