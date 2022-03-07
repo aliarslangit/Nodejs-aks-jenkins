26a550604329d3aaa0ba431fa0379b34a72bd327
Ali Arslan <aliarslanmushtaqali@gmail.com>
2022-03-07 12:31:16 +0500

    Update helm.sh

--- scripts/helm.sh
+++ scripts/helm.sh
@@ -5,10 +5,3 @@ wget https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
 tar xvf helm-v3.4.1-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin
 
-<<'COMMENTS'
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-chmod 700 get_helm.sh
-chown jenkins:jenkins get_helm.sh
-./get_helm.sh
-COMMENTS
-
