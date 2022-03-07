e1a2ca94cbd0168a278e1b304606f06b5bed1ad5
Ali Arslan <aliarslanmushtaqali@gmail.com>
2022-03-07 13:50:01 +0500

    Install Helm

--- scripts/ingress.sh
+++ scripts/ingress.sh
@@ -2,5 +2,8 @@
 
 
 # deploy an NGINX ingress controller
-git clone https://github.com/scriptcamp/nginx-ingress-controller.git
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
+
+helm repo add nginx-stable https://helm.nginx.com/stable
+helm repo update
+helm install ingress-nginx nginx-stable/nginx-ingress
+helm install ingress-nginx nginx-stable/nginx-ingress --set rbac.create=true
