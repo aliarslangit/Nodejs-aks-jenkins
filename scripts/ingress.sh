4fd16f65ab7f4bf94382ea6539c11639d70ceaf1
Ali Arslan <aliarslanmushtaqali@gmail.com>
2022-03-07 12:36:03 +0500

    Update ingress.sh

--- scripts/ingress.sh
+++ scripts/ingress.sh
@@ -2,4 +2,5 @@
 
 
 # deploy an NGINX ingress controller
+git clone https://github.com/scriptcamp/nginx-ingress-controller.git
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
