8f978fd0f66ec017c07070014345b0782189c3ee
Ali Arslan <100302855+aliarslangit@users.noreply.github.com>
2022-03-07 13:54:26 +0500

    Update ingress.sh

--- scripts/ingress.sh
+++ scripts/ingress.sh
@@ -3,7 +3,4 @@
 
 # deploy an NGINX ingress controller
 
-helm repo add nginx-stable https://helm.nginx.com/stable
-helm repo update
-helm install ingress-nginx nginx-stable/nginx-ingress
-helm install ingress-nginx nginx-stable/nginx-ingress --set rbac.create=true
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.0/deploy/static/provider/cloud/deploy.yaml
